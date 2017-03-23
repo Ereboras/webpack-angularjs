1900ebbefb3755261eb6225e007333dc29309f90
Mohamed AZZOUZ <mohamed.azzouz@al-enterprise.com>
2017-03-23 09:11:49 +0100

    Add library

--- app/app.js
+++ app/app.js
@@ -2,12 +2,21 @@ console.log(" [authModule] [bundle.js] Instanciate angular module");
 
 // Import our librarie(s)
 import angular from 'angular';
-//import uirouter from 'angular-ui-router';
+import ngAnimate from 'angular-animate';
+//import uiRouter from 'angular-ui-router';
+//import notification from 'angular-ui-notification';
+//import ngCookies from 'angular-cookies';
+//import uiBootstrap from 'angular-ui-bootstrap';
+//import draganddrop from 'angular-draganddrop';
 
 // Import route for authModule & the ts files needed for this module
 import routes from './app.route';
 import home from './components/home/home';
 
-angular.module("authModule", ['ui.router'])
-    .config(routes)
+angular.module("authModule", [
+    "notification",
+	"ngCookies",
+	ngAnimate,
+    "ui.router"
+    ]).config(routes)
     .component("homeComponent", home);
